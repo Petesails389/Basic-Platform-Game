c1f04ac6ce322866a6c9526d56f4319c7a363a12
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 16:58:25 +0100

    cleaned up movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,36 +7,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] new Rigidbody2D rigidbody;
     [SerializeField] Collider2D groundDetectorCollider;
     [SerializeField] LayerMask groundLayer;
-    [SerializeField] float acelleration;
     [SerializeField] float maxVelocity;
     [SerializeField] float jumpForce;
-    [SerializeField] float drag;
 
     private bool isGrounded;
 
     void Update()
     {
         isGrounded = groundDetectorCollider.IsTouchingLayers(groundLayer);
-        HorizontalDrag();
     }
 
     public void Move(float _direction)
     {
-        if(rigidbody.velocity.x < maxVelocity && rigidbody.velocity.x > (maxVelocity * -1f)){
-            rigidbody.AddForce(new Vector2(acelleration*_direction,0));
-        }
+        rigidbody.velocity = new Vector2(maxVelocity*_direction, rigidbody.velocity.y);
     }
 
     public void Jump()
     {
         if(isGrounded){
             rigidbody.AddForce(new Vector2(0,jumpForce));
-        }
-    }
-
-    void HorizontalDrag(){
-        if (rigidbody.velocity.x != 0){
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x / drag, rigidbody.velocity.y);
         }
     }
 }
